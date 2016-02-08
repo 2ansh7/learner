cbdaa641fe8a31f8f2aedb1c7e39917024474ba4
2ansh7 <1127ansasharma@gmail.com>
2016-02-08 04:25:00 -0500

    script5.sh added

---
+++ script5.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# this is a master file
+# this script use for system health checkup
+# Created by anmol sharma (1127ansasharma@gmail.com)
+echo `whoami` >> `pwd`/my.log
+echo `date` >> `pwd`/my.log
+free -m | awk 'NR==2{printf "Memory Usage: %s/%sMB (%.2f%%)\n", $3,$2,$3*100/$2 }' | tee -a `pwd`/my.log
+df -h | awk '$NF=="/"{printf "Disk Usage: %d/%dGB (%s)\n", $3,$2,$5}' | tee -a `pwd`/my.log
+top -bn1 | grep load | awk '{printf "CPU Load: %.2f\n", $(NF-2)}' | tee -a `pwd`/my.log
+Used=`free -m | awk 'NR==2{printf"%s",$3}'`
+Total=`free -m | awk 'NR==2{printf"%s",$2}'`
+if [ "$(( $Used + 500 ))" -gt "$Total" ]; then echo -e "Memory Usage: High \nMemory Cache needs to flush. Contact your Administrator." | tee -a `pwd`/my.log; fi  
+echo >> `pwd`/my.log
